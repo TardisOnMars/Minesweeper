ce1380d40f248b0c58b7e3a5869888323b2cb699
TardisOnMars <sososmile99@gmail.com>
2017-05-11 12:11:48 +0200

    Test pull

--- Test.java
+++ Test.java
@@ -1,26 +1,18 @@
 
 public class Test{
 	public static void main(String[] args){
-		java.util.Scanner sc = new java.util.Scanner(System.in);
+		
 		
 		Demineur a = new Demineur();
-
 		a.genereMines();
 		a.genereNombres();
-		System.out.println();
 		a.afficherChamps();
-		
-		while (true){
-			System.out.println("Donnez 2 entiers delimite par un espace");
-			
-			//String s1 = s.substring(0, 1);
-			//String s2 = s.substring(3, 5);
-			
-			int i = sc.nextInt();
-			int j = sc.nextInt();
-			
-			a.decouvreMine(i,j);
-			a.afficherChampsBoolean();
-		}
+		a.afficherChampsPlayer();
+		a.afficherBoolean();
+		a.decouvreMine(1,1);
+		a.afficherChamps();
+		a.afficherBoolean();
+		a.afficherChampsPlayer();
+
 	}
 }
